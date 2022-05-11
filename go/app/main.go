2e877966d66ba74f3328a23147154f3d5b312393
ysaito <shiva.lts239@gmail.com>
2022-05-11 20:10:36 +0900

    feat(go/app/main.go): Read and Add POST data to json file
    Changed to use nested structures

--- go/app/main.go
+++ go/app/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"encoding/json"
 	"net/http"
 	"os"
 	"path"
@@ -21,6 +21,15 @@ type Response struct {
 	Message string `json:"message"`
 }
 
+type Items struct {
+	Items []Item `json:"items"`
+}
+
+type Item struct {
+	Name string `json:"name"`
+	Category string `json:"category"`
+}
+
 func root(c echo.Context) error {
 	res := Response{Message: "Hello, world!"}
 	return c.JSON(http.StatusOK, res)
@@ -30,6 +39,9 @@ func addItem(c echo.Context) error {
 	// Get form data
 	name := c.FormValue("name")
 	c.Logger().Infof("Receive item: %s", name)
+
+	category := c.FormValue("category")
+	c.Logger().Infof("Receive item: %s", category)
 
 	message := fmt.Sprintf("item received: %s", name)
 	res := Response{Message: message}
@@ -41,27 +53,43 @@ func addItem(c echo.Context) error {
 		c.Logger().Infof(err.Error())
 	}
 
+	// // Close json file using defer
+	defer f.Close()
+
 	// // Read data to json file
-	raw_data, err := os.ReadFile("items.json")
+	items := []Item{}
+	save_items := Items{items}
+
+	read_items_byte, err := os.ReadFile("items.json")  // read_items_byte is []byte
+	if err != nil {
+		c.Logger().Infof(err.Error())
+	}
+	c.Logger().Infof(string(read_items_byte)) // TODO: 後で消す
+
+	if len(read_items_byte) != 0 {
+	// // Parse read_items_byte
+		err = json.Unmarshal(read_items_byte, &save_items)
+		if err != nil {
+			c.Logger().Infof(err.Error())
+		}
+		c.Logger().Infof(save_items.Items[0].Name) // TODO: 後で消す
+	}
+
+	// // Add data to decode_data
+	append_item := Item{ Name: name, Category: category}
+	save_items.Items = append(save_items.Items, append_item)
+
+	// // Set indent
+	encode_items, err := json.MarshalIndent(save_items, "", " ")
 	if err != nil {
 		c.Logger().Infof(err.Error())
 	}
 
-	var decode_data map[string]interface{}
-	json.Unmarshal([]byte(raw_data), &decode_data)
-
-	c.Logger().Infof("Mapping!!!: %s", fmt.Sprintf("%s", decode_data["test"])) // TODO: 後で消す
-	items := decode_data["items"].([]interface{})
-	c.Logger().Infof("\"name\": %s", fmt.Sprintf("%s", items[0].(map[string]interface{})["name"])) // TODO: 後で消す
-	c.Logger().Infof("\"category\": %s", fmt.Sprintf("%s", items[0].(map[string]interface{})["category"])) // TODO: 後で消す
-
-	// // Add data to json file
-
-	// // Close json file
-	if err := f.Close(); err != nil {
+	// // Write decode_data to json file
+	err = os.WriteFile("items.json", encode_items, 0644)
+	if err != nil {
 		c.Logger().Infof(err.Error())
 	}
-
 	return c.JSON(http.StatusOK, res)
 }
 
